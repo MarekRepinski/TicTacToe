0c574e97fb4748795698e3a69aa7fcdab38c4bf2
Marek Repinski <Repinski@hotmail.com>
2020-12-15 16:02:44 +0100

    First page and segue to second page

---
+++ TicTacToe/TicTacToeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TicTacToeViewController.swift
+//  TicTacToe
+//
+//  Created by Marek Repinski on 2020-12-15.
+//
+
+import UIKit
+
+class TicTacToeViewController: UIViewController {
+    var noOfPlayers = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let segueToGameTwoP = "segueToGameTwoP"
+    let segueToGameOneP = "segueToGameTwoP"
+    var noOfPlayers = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onePlayer(_ sender: UIButton) {
+        performSegue(withIdentifier: segueToGameOneP, sender: self)
+    }
+    
+    @IBAction func twoPlayer(_ sender: UIButton) {
+        performSegue(withIdentifier: segueToGameTwoP, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueToGameTwoP {
+            let destinationVC = segue.destination as! TicTacToeViewController
+            destinationVC.noOfPlayers = noOfPlayers
+        }
+    }
+    
+    @IBAction func unwindToStartView(segue: UIStoryboardSegue){}
 }
 
