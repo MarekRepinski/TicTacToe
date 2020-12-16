e5e0963df6582cce94153a9620b7690a2dbee7c5
Marek Repinski <Repinski@hotmail.com>
2020-12-16 19:01:14 +0100

    Starting on new class

---
+++ TicTacToe/TicTackToeGame.swift
@@ -0,0 +1,22 @@
+//
+//  TicTackToeGame.swift
+//  TicTacToe
+//
+//  Created by Marek Repinski on 2020-12-16.
+//
+
+import Foundation
+
+class TicTackToeGame {
+    var isXstarting: Bool
+    private var gameTurn: Int
+    
+    init(isXstarting: Bool = true){
+        self.isXstarting = isXstarting
+        self.gameTurn = 0
+    }
+    
+    func nextMove(y: Int, x: Int){
+        
+    }
+}
